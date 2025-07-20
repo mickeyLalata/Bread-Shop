ef1d5a2488730a9079432b7bcf90a3a3cc26818f
mickeyLalata <mckyl1108@gmail.com>
2025-07-21 01:52:57 +0800

    added button function

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,24 @@
 import './template.css';
+
+import homepage from './homepage.js';
+
+function navigation(){
+    const buttons = document.querySelectorAll('nav button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const page = e.target.textContent.toLowerCase();
+
+            if (page === "home"){
+                homepage();
+            } else if (page === "menu") {
+                menupage();
+            } else if (page === "about") {
+                aboutpage();
+            }
+        });
+    });
+}
+
+homepage();
+navigation();
